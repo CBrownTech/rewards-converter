9cb73939696c0a4361c25fa31e65eba4b110f83c
CBrownTech <cbrown46@usf.edu>
2023-04-27 07:52:24 -0400

    New Java Class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,20 +2,24 @@ public class RewardValue {
     private double cashValue;
     private int milesValue;
 
+// Constructor that accepts a cash value
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
         this.milesValue = (int) Math.round(cashValue / 0.0035);
     }
 
+    // Constructor that accepts a value in miles
     public RewardValue(int milesValue){
         this.milesValue = milesValue;
         this.cashValue = milesValue * 0.0035;
     }
 
+    // Method that returns the cash value of the RewardValue class
     public double getCashValue() {
         return this.cashValue;
     }
 
+    // Method that returns how many miles the 'RewardValue' is worth
     public int getMilesValue() {
         return this.milesValue;
     }
